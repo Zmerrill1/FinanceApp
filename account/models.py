501f63d533125e2b28711b1d1706f4ef7e385e8a
Zmerrill1 <zach_merrill1@outlook.com>
2024-12-04 17:13:35 -0700

    Saved the models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    account_type = models.CharField(max_length = 50) #i.e, "checking" or "savings" or "credit card", etc.
+
+    def __str__(self) -> str:
+        return self.name
--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from account.models import Account
+from category.models import Category
 
-# Create your models here.
+class Transaction(models.Model):
+    TRANSACTION_TYPE = [
+        ('INCOME', 'Income'),
+        ('EXPENSE', 'Expense'),
+    ]
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPE)
+    description = models.TextField(blank=True, null=True)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.transaction_type} - {self.amount} ({self.category})"
