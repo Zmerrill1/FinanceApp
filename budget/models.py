26ac3b4ff53e1223ded5e8c37a99ca1e8f34ea05
Zmerrill1 <zach_merrill1@outlook.com>
2024-12-04 19:40:26 -0700

    Budget Model Update
    Updated to have a budget app/model.

--- budget/models.py
+++ budget/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from category.models import Category
+from transactions.models import Transaction
 
 class Budget(models.Model):
     name = models.CharField(max_length=100)
@@ -14,7 +15,10 @@ class BudgetItem(models.Model):
     budget = models.ForeignKey(Budget, related_name='items', on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     planned_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    spent_amount = models.DecimalField(max_digits=10, decimal_places=2) #not sure why this is giving me an error?
+    spent_amount = models.DecimalField(max_digits=10, decimal_places=2) #not sure why this is giving me an error when I set a default value of 0?
+
+    #adding a relationshop to the Transaction model
+    transactions = models.ManyToManyField(Transaction, related_name='budget_items')
 
     def __str__(self) -> str:
         return f"{self.category.name} - {self.budget.name}"
