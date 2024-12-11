9a65a77bef577477f42389384033e94d0224579f
Zmerrill1 <zach_merrill1@outlook.com>
2024-12-10 19:26:41 -0700

    Adding Views and Urls
    Added more views to add, edit, delete, and view transactions. Tried to add the html files and urls.

--- budget/forms.py
+++ budget/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+from .models import Transaction
 
 class CSVUploadForm(forms.Form):
     file = forms.FileField(label='Upload a CSV file')
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['amount', 'date', 'description',]
--- budget/views.py
+++ budget/views.py
@@ -1,28 +1,65 @@
 import csv
-from django.shortcuts import render
+import io
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from .forms import CSVUploadForm
+# from django.contrib.auth.decorators import login_required
+from .forms import CSVUploadForm, TransactionForm
 from .models import Transaction
 
+# @login_required
 def upload_csv(request):
     if request.method == 'POST':
         form = CSVUploadForm(request.POST, request.FILES)
         if form.is_valid():
             csv_file = request.FILES['file']
 
-        try:
-            with open(csv_file, 'r') as f:
-                reader = csv.DictReader(f, delimiter=';')
-                for row in reader:
-                    # print(row)
+            csv_data = csv_file.read().decode('utf-8')
+            io_string = io.StringIO(csv_data)
+            reader = csv.DictReader(io_string, delimiter=';')
 
-                    Transaction.objects.create(
-                        date = row['Transaction Date'],
-                        description = row['Description'],
-                        amount = row['Amount'],
-                    )
-                
-                messages.success(request, 'CSV files uploaded and processed successfully')
-        except Exception as e:
-            messages.error(request, f'An error occurred: {e}')
+            transactions = []
+            for row in reader:
+                   transactions.append(Transaction(
+                          account = row['Account'], #Account probably wouldn't be listed on the csv, since the file will come from a single CSV file. But would be the same for each item in a CSV upload
+                          date = row['Transaction Date'],
+                          description = row['Description'],
+                          amount = row['Amount'],
+                   ))
 
+            Transaction.objects.bulk_create(transactions)
+            messages.success(request, f'CSV files uploaded and processed {len(transactions)} successfully')
+    
+    return render(request, 'upload.html', {'form': form}) 
+
+def add_transaction(request):
+    if request.method == 'POST':
+          form = TransactionForm(request.POST)
+          if form.is_valid():
+               form.save()
+               return redirect('transaction_list')
+    else:
+        form = TransactionForm()
+
+    return render(request, 'add_transaction.html', {'form': form})
+
+def edit_transaction(request, transaction_id): #would the database automatically generate a transaction_id or do I need to add that do the model?
+    transaction = get_object_or_404(Transaction, id =transaction_id)
+
+    if request.method == 'POST':
+        form = TransactionForm(request.POST, instance=transaction)
+        if form.is_valid():
+            form.save()
+            return redirect('transaction_list')
+    else:
+         form = TransactionForm(instance=transaction)
+          
+    return render(request, 'edit_transaction.html', {'form': form, 'transaction': transaction})
+
+def delete_transaction(request, transaction_id):
+     transaction = get_object_or_404(Transaction, id=transaction_id)
+     transaction.delete()
+     return redirect('transaction_list')
+
+def transaction_list(request):
+     transactions = Transaction.objects.all()
+     return render(request, 'transaction_list.html', {'transactions': transactions})
